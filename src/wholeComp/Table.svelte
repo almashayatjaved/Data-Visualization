64fe7fa4c018acb381d9d60e310166ec4ea9c2a4
AlMass <radiantDev03@gmail.com>
2024-02-06 16:32:54 +0500

    alignment changes

--- src/wholeComp/Table.svelte
+++ src/wholeComp/Table.svelte
@@ -10,34 +10,28 @@
 </script>
 
 <main class="container mt-5">
-  <div class=" row d-flex flex-row  justify-content-center align-items-center">
-    <!-- Left section for description -->
-    <div class="col-md-6" >
-      <div class="background" style="width: 70%;">
-        <div class="card-body text-start">
-          <h3 class="card-title text-start my-4">Table in Data visualization</h3>
-            <p class="card-text text-start">{description}</p>
+  <div class="row d-flex flex-row justify-content-center align-items-center">
+      
+      <div class="col-md-6">
+          <div class="textColor">
+              <div class="card-body">
+                <h3 class="card-title text-start my-4">Table in Data visualization</h3>
+                <p class="card-text text-start">{description}</p>
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="col-md-6">
+            <div class="">
+                <!-- svelte-ignore a11y-img-redundant-alt -->
+                <!-- <img src="/images/table.png" class="card-img-top" alt="Image" /> -->
+                <Table />
+            </div>
+        </div>
     </div>
-
-    <!-- Right section for image -->
-    <div class="col-md-6">
-      <div class="">
-        <!-- svelte-ignore a11y-img-redundant-alt -->
-        <!-- <img src="/images/table.png" class="card-img-top" alt="Image" /> -->
-        <Table />
-      </div>
-    </div>
-  </div>
 </main>
 
 <style>
-  /* .background {
-    background-color: #06133d;
-  } */
   .container{
-    max-width: fit-content;
     font-family:  system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
   h3{
